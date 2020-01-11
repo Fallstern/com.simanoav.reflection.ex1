d2f5ac72344af44faa910b9d1441a27b034d36d8
Andrii <simanoav@gmail.com>
2020-01-11 20:20:09 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.lang.reflect.Method;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Tested ts = new Tested();
+        Class cls = ts.getClass();
+        try {
+            Method[] mtd = cls.getDeclaredMethods();
+            for (Method m: mtd) {
+                if (m.isAnnotationPresent(Test.class)){
+                    Test test = m.getDeclaredAnnotation(Test.class);
+                    m.invoke(ts,test.a(),test.b());
+                }
+            }
+        } catch (NoSuchMethodException e){
+            System.out.println(e.toString());
+        }
+
+
+
+
+
+
+    }
+}
---
+++ src/Tested.java
@@ -0,0 +1,6 @@
+public class Tested {
+    @Test(a=5, b=15)
+    public static void test (int a, int b){
+        System.out.println(a+b);
+    }
+}
